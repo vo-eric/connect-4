14550936c550f4ffa7c07f0d62393e43adc38635
Eric Vo <vo.eric@gmail.com>
2025-06-02 13:00:53 -0400

    Add move function with related tests

--- src/game.test.ts
+++ src/game.test.ts
@@ -1,5 +1,5 @@
-import { test, assert } from 'vitest';
-import { initializeGame } from './game';
+import { test, assert, describe, expect } from 'vitest';
+import { determinePlayer, initializeGame, move } from './game';
 
 test('game initializes a valid game state', () => {
   assert.deepEqual(initializeGame(), {
@@ -11,6 +11,116 @@ test('game initializes a valid game state', () => {
       [null, null, null, null, null, null, null],
       [null, null, null, null, null, null, null],
     ],
-    currentPlayer: 'black',
+    currentPlayer: 'B',
+  });
+});
+
+describe('game correctly updates the current player', () => {
+  test('Player R goes to B', () => {
+    const currentPlayer = 'B';
+    const newPlayer = determinePlayer(currentPlayer);
+    expect(newPlayer).toBe('R');
+  });
+
+  test('Player B goes to R', () => {
+    const currentPlayer = 'R';
+    const newPlayer = determinePlayer(currentPlayer);
+    expect(newPlayer).toBe('B');
+  });
+});
+
+describe('a player can place a piece', () => {
+  test('a player can place a piece on an empty column', () => {
+    const game = initializeGame();
+    const newGameState = move(game.board, 2, 'B');
+
+    assert.deepEqual(newGameState, {
+      board: [
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, 'B', null, null, null, null],
+      ],
+      currentPlayer: 'R',
+    });
+  });
+
+  test('a player can place a piece on an column with an existing piece', () => {
+    let game = initializeGame();
+    game = move(game.board, 2, 'B');
+    game = move(game.board, 2, 'R');
+
+    assert.deepEqual(game, {
+      board: [
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, 'R', null, null, null, null],
+        [null, null, 'B', null, null, null, null],
+      ],
+      currentPlayer: 'B',
+    });
+  });
+
+  test('a player cannot place a piece in a full column', () => {
+    let game = initializeGame();
+    game.board = [
+      [null, null, 'R', null, null, null, null],
+      [null, null, 'B', null, null, null, null],
+      [null, null, 'R', null, null, null, null],
+      [null, null, 'B', null, null, null, null],
+      [null, null, 'R', null, null, null, null],
+      [null, null, 'B', null, null, null, null],
+    ];
+    game = move(game.board, 2, 'B');
+
+    assert.deepEqual(game, {
+      board: [
+        [null, null, 'R', null, null, null, null],
+        [null, null, 'B', null, null, null, null],
+        [null, null, 'R', null, null, null, null],
+        [null, null, 'B', null, null, null, null],
+        [null, null, 'R', null, null, null, null],
+        [null, null, 'B', null, null, null, null],
+      ],
+      currentPlayer: 'B',
+    });
+  });
+
+  test('a player cannot place out of bounds', () => {
+    let game = initializeGame();
+    game = move(game.board, -1, 'B');
+
+    assert.deepEqual(game, {
+      board: [
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+      ],
+      currentPlayer: 'B',
+    });
+  });
+
+  test('a player cannot place out of bounds', () => {
+    let game = initializeGame();
+    game = move(game.board, 8, 'B');
+
+    assert.deepEqual(game, {
+      board: [
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+      ],
+      currentPlayer: 'B',
+    });
   });
 });
--- src/game.ts
+++ src/game.ts
@@ -8,7 +8,7 @@ State:
 
 */
 
-export type Player = 'red' | 'black';
+export type Player = 'R' | 'B';
 export type Cell = Player | null;
 export type Board = Cell[][];
 export type Winner = Player | undefined;
@@ -23,6 +23,47 @@ export const initializeGame = (): Game => {
 
   return {
     board: newBoard,
-    currentPlayer: 'black',
+    currentPlayer: 'B',
+  };
+};
+
+export const determinePlayer = (currentPlayer: Player): Player => {
+  return currentPlayer === 'B' ? 'R' : 'B';
+};
+/*
+move(board): Board 
+Select a column
+From there, go down rows (starting at 0) until the next row is either the bottom or is NOT null
+  If that is satisfied, set the current cell to the current player
+*/
+
+export const move = (
+  board: Board,
+  column: number,
+  currentPlayer: Player
+): Game => {
+  const newBoard = structuredClone(board);
+
+  //exceptions
+  if (
+    column < 0 ||
+    column >= newBoard[0].length ||
+    newBoard[0][column] !== null
+  ) {
+    return { board, currentPlayer };
+  }
+
+  for (let row = 0; row <= newBoard.length - 1; row++) {
+    if (row === newBoard.length - 1 || newBoard[row + 1][column] !== null) {
+      newBoard[row][column] = currentPlayer;
+      break;
+    }
+  }
+
+  // console.log('updated', newBoard);
+
+  return {
+    board: newBoard,
+    currentPlayer: determinePlayer(currentPlayer),
   };
 };
