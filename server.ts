575f39eecf9b1127d6d79ce8f7f62ee38ca72d25
Eric Vo <vo.eric@gmail.com>
2025-06-04 13:53:15 -0400

    Add route to make a move

--- server.ts
+++ server.ts
@@ -3,16 +3,23 @@ import ViteExpress from 'vite-express';
 import ConnectFourAPI from './api/connectFour';
 
 const app = express();
+app.use(express.json());
 const connectFour = new ConnectFourAPI();
 
-app.get('/api/games/:id', async (req, res) => {
+app.get('/api/game/:id', async (req, res) => {
   const game = await connectFour.getGame(req.params.id);
   return res.json(game);
 });
 
-app.post('/api/game', async (req, res) => {
+app.post('/api/game', async (_, res) => {
   const newGame = await connectFour.createGame();
-  res.json(newGame);
+  return res.json(newGame);
+});
+
+app.post('/api/game/:id/move', async (req, res) => {
+  const updatedGame = await connectFour.move(req.params.id, req.body.column);
+
+  return res.json(updatedGame);
 });
 
 ViteExpress.listen(app, 3000, () => console.log('Server is listening...'));
