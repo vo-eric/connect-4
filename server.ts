6e812116bbe178b2128d11545cb136d155b05f64
Eric Vo <vo.eric@gmail.com>
2025-06-04 12:23:38 -0400

    Add /game post request

--- server.ts
+++ server.ts
@@ -1,8 +1,15 @@
 import express from 'express';
 import ViteExpress from 'vite-express';
+import ConnectFourAPI from './api/connectFour';
 
 const app = express();
 
 app.get('/message', (_, res) => res.send('Hello from express!'));
+
+app.post('/game', async (req, res) => {
+  const connectFour = new ConnectFourAPI();
+  const newGame = await connectFour.createGame();
+  res.json(newGame);
+});
 
 ViteExpress.listen(app, 3000, () => console.log('Server is listening...'));
