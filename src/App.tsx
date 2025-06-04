eea4de0271f10e8e35234967af5fc1ad8414c269
Eric Vo <vo.eric@gmail.com>
2025-06-04 18:43:27 -0400

    Add fun winning/tie animations

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import clsx from 'clsx';
 import useSound from 'use-sound';
 import pirHorn from '../public/the-price-is-right-losing-horn.mp3';
 import { ConnectFourClientAPI } from '../api/connectFour';
+import Celebration from './Celebration';
 
 function App() {
   const api = useMemo(() => new ConnectFourClientAPI(), []);
@@ -37,13 +38,49 @@ function App() {
   const renderFinishedState = () => {
     switch (gameState!.winningPlayer) {
       case 'B':
-        return <p className='text-4xl text-black'>Black wins!</p>;
+        return <Celebration color='#000000' />;
       case 'R':
-        return <p className='text-4xl text-red-900'>Red wins!</p>;
+        return <Celebration color='#82181a' />;
       case 'tie':
-        return <p className='text-4xl text-green'>It's a tie!</p>;
+        return <Celebration color='#5C4033' />;
       default:
-        return null;
+        return;
+    }
+  };
+
+  /*
+  TODO: Refactor
+  */
+  const renderGameMessage = (gameState: Game) => {
+    if (!gameState.winningPlayer) {
+      return (
+        <p className='text-4xl'>
+          Current Player:{' '}
+          <span
+            className={clsx({
+              'text-black': gameState.currentPlayer === 'B',
+              'text-red-900': gameState.currentPlayer === 'R',
+            })}
+          >
+            {gameState.currentPlayer === 'B' ? 'Black' : 'Red'}
+          </span>
+        </p>
+      );
+    } else {
+      return (
+        <p className='text-4xl'>
+          <span
+            className={clsx({
+              'text-black': gameState.currentPlayer === 'B',
+              'text-red-900': gameState.currentPlayer === 'R',
+            })}
+          >
+            {gameState.winningPlayer === 'tie'
+              ? 'No one wins. What a shame'
+              : `${gameState.winningPlayer} wins!`}
+          </span>
+        </p>
+      );
     }
   };
 
@@ -59,62 +96,58 @@ function App() {
   }
 
   return (
-    <div className='flex flex-col gap-8 text-center'>
-      <p className='text-4xl'>
-        Current Player:{' '}
-        <span
-          className={clsx({
-            'text-black': gameState.currentPlayer === 'B',
-            'text-red-900': gameState.currentPlayer === 'R',
+    <>
+      <div className='flex flex-col gap-8 text-center'>
+        {renderGameMessage(gameState)}
+        <div
+          className={clsx('flex-col gap-4 bg-blue-800 p-2 rounded-lg ', {
+            'pointer-events-none': gameState.winningPlayer,
           })}
         >
-          {gameState.currentPlayer === 'B' ? 'Black' : 'Red'}
-        </span>
-      </p>
-      <div
-        className={clsx('flex-col gap-4 bg-blue-800 p-2 rounded-lg ', {
-          'pointer-events-none': gameState.winningPlayer,
-        })}
-      >
-        {gameState.board.map((row, i) => {
-          return (
-            <div className='flex p-1 gap-2'>
-              {row.map((_, j) => {
-                return (
-                  <div
-                    className={clsx(
-                      'rounded-full h-12 w-12 transition duration-300',
-                      {
-                        'bg-white':
-                          gameState.board[i][j] === null && hoveredColumn !== j,
-                      },
-                      {
-                        'bg-gray-300':
-                          hoveredColumn === j && gameState.board[i][j] === null,
-                      },
-                      { 'bg-black': gameState.board[i][j] === 'B' },
-                      { 'bg-red-900': gameState.board[i][j] === 'R' }
-                    )}
-                    onClick={() => handleClick(j)}
-                    onMouseEnter={() => setHoveredColumn(j)}
-                    onMouseLeave={() => setHoveredColumn(null)}
-                  />
-                );
-              })}
-            </div>
-          );
-        })}
+          {gameState.board.map((row, i) => {
+            return (
+              <div className='flex p-1 gap-2'>
+                {row.map((_, j) => {
+                  return (
+                    <div
+                      className={clsx(
+                        'rounded-full h-12 w-12 transition duration-300',
+                        {
+                          'bg-white':
+                            gameState.board[i][j] === null &&
+                            hoveredColumn !== j,
+                        },
+                        {
+                          'bg-gray-300':
+                            hoveredColumn === j &&
+                            gameState.board[i][j] === null,
+                        },
+                        { 'bg-black': gameState.board[i][j] === 'B' },
+                        { 'bg-red-900': gameState.board[i][j] === 'R' }
+                      )}
+                      onClick={() => handleClick(j)}
+                      onMouseEnter={() => setHoveredColumn(j)}
+                      onMouseLeave={() => setHoveredColumn(null)}
+                    />
+                  );
+                })}
+              </div>
+            );
+          })}
+        </div>
+        <button
+          className='transition duration-300 rounded-lg bg-white py-2 px-4 text-bg-blue hover:bg-bg-blue hover:text-white hover:border-1 hover:border-white cursor-pointer border-1 border-bg-blue font-semibold'
+          onClick={handleNewGameClick}
+        >
+          {gameState.winningPlayer ? 'New Game' : 'Reset Game'}
+        </button>
+        {gameState.winningPlayer && (
+          <div className='winner min-h-10 absolute'>
+            {renderFinishedState()}
+          </div>
+        )}
       </div>
-      <button
-        className='transition duration-300 rounded-lg bg-white py-2 px-4 text-bg-blue hover:bg-bg-blue hover:text-white hover:border-1 hover:border-white cursor-pointer border-1 border-bg-blue font-semibold'
-        onClick={handleNewGameClick}
-      >
-        {gameState.winningPlayer ? 'New Game' : 'Reset Game'}
-      </button>
-      {gameState.winningPlayer && (
-        <div className='winner min-h-10'>{renderFinishedState()}</div>
-      )}
-    </div>
+    </>
   );
 }
 
